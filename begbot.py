3bb3e4e1683b0ee3e6af0a7d2cd07dfb92fddb6e
Lumiukko <makko@brainscorch.net>
2016-06-30 11:40:35 +0200

    Added basic DB persistence and permission control.
    Now with `\session` command.
    Also added docstrings to the extended emoji class.

--- begbot.py
+++ begbot.py
@@ -7,40 +7,117 @@ This is the Bouncing Egg Telegram Bot.
 # from telegram.ext import MessageHandler, Filters, InlineQueryHandler
 from telegram.ext import Updater, CommandHandler
 import logging
+import os
 import json
 import ts3.query
 from botconfig import BotConfig
 from extended_emoji import ExtendedEmoji as Emoji
+import sqlite3
 
 
 class BEGBot:
+
+    version = "0.1"
 
     def __init__(self):
         self.cfg = BotConfig(json.load(open("config.json", "r")))
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
         self.logger = logging.getLogger(__name__)
+        self.init_db()
+        self.logger.info("SessionID: {}".format(self.cfg.session_id))
+
         self.updater = Updater(self.cfg.token)
         self.dp = self.updater.dispatcher
         self.dp.add_error_handler(self.error)
         # self.dp.add_handler(InlineQueryHandler(self.inline_query))
         # self.dp.add_handler(MessageHandler([Filters.text], self.message))
         self.dp.add_handler(CommandHandler("ts3", self.ts3_info))
+        self.dp.add_handler(CommandHandler("session", self.session_info))
 
     def start(self):
+        """
+        Makes the bot start polling for messages.
+
+        @return: None
+        """
         self.updater.start_polling()
         self.updater.idle()
 
     def error(self, update, error):
-        self.logger.warn("Error: Update={}, Error={}".format(update, error))
+        """
+        Sends a warning to the logger that reports on an error occurred.
+
+        @param update: The update at which the error occurred.
+        @param error: The error that occurred
+        @return: None
+        """
+        self.logger.warn("Update={}, Error={}".format(update, error))
+
+    def is_beg(self, telegram_id):
+        """
+            Checks if the user with the given telegram id is tagged as a BEG member.
+
+            @param telegram_id The users telegram id.
+        """
+        con = sqlite3.connect(self.cfg.db_file)
+        c = con.cursor()
+        c.execute("select count(id) from user where beg=1 and telegram_id=?", (telegram_id, ))
+        (result, ) = c.fetchone()
+        c.close()
+        con.close()
+        return result
+
+    def is_admin(self, telegram_id):
+        """
+            Checks if the user with the given telegram id has administrator permissions.
+
+            @param telegram_id The users telegram id.
+        """
+        con = sqlite3.connect(self.cfg.db_file)
+        c = con.cursor()
+        c.execute("select count(id) from user where admin=1 and telegram_id=?", (telegram_id, ))
+        (result, ) = c.fetchone()
+        c.close()
+        con.close()
+        return result
+
+    def session_info(self, bot, update):
+        """
+        Gets information (start time, end time, and duration) about the current session.
+
+        @param bot: The Telegram bot instance.
+        @param update: The update that triggered the command.
+        @return: True if successful, False otherwise.
+        """
+        if not self.is_admin(update.message.from_user.id):
+            self.send_message_admin_only(bot, update)
+            return False
+
+        con = sqlite3.connect(self.cfg.db_file)
+        c = con.cursor()
+        c.execute("select id, start, end, strftime('%s', end) - strftime('%s', start) as duration "
+                  "from session where id=?",
+                  (self.cfg.session_id, ))
+        result = c.fetchone()
+        c.close()
+        con.close()
+        response = "BEGBot [{}], Session: id={}, start={}, lastka={}, duration={}".format(
+            self.version, result[0], result[1], result[2], result[3])
+        bot.sendMessage(update.message.chat_id, text=response)
+        return True
 
     def ts3_info(self, bot, update):
         """
         Connects to a Teamspeak 3 server, gets client information, and responds with a channel overview.
 
         @param bot: The Telegram bot instance.
-        @param update: The Upate that triggered the command.
+        @param update: The update that triggered the command.
         @return: Returns True if it was successful, and False if it was not.
         """
+
+        if not self.is_beg(update.message.from_user.id):
+            self.send_message_beg_only(bot, update)
+            return False
 
         try:
             ts3_connection = ts3.query.TS3Connection(self.cfg.ts3_srv)
@@ -56,12 +133,13 @@ class BEGBot:
                 if not client["client_nickname"].startswith("{} from ".format(self.cfg.ts3_usr)):
                     channels[client["cid"]] = 0
                     client_muted = client["client_input_muted"] == "1"
-                    clients.append((client["client_nickname"], client["cid"], client_muted, client["client_country"]))
+                    clients.append((client["client_nickname"], client["cid"],
+                                    client_muted, client["client_country"]))
 
             if len(clients) == 0:
-                response = "{}{} TeamSpeak 3 {}{}\n There's nobody online {}"\
-                    .format(Emoji.HEAVY_MINUS_SIGN, Emoji.HEAVY_MINUS_SIGN, Emoji.HEAVY_MINUS_SIGN,
-                            Emoji.HEAVY_MINUS_SIGN, Emoji.WORRIED_FACE)
+                response = "{}{} TeamSpeak 3 {}{}\n There's nobody online {}".format(
+                    Emoji.HEAVY_MINUS_SIGN, Emoji.HEAVY_MINUS_SIGN,
+                    Emoji.HEAVY_MINUS_SIGN, Emoji.HEAVY_MINUS_SIGN, Emoji.WORRIED_FACE)
                 bot.sendMessage(update.message.chat_id, text=response)
                 return True
 
@@ -75,7 +153,7 @@ class BEGBot:
 
             entries = []
             for c in channels:
-                clientlines = []
+                client_lines = []
                 for client in range(0, len(channels[c]["clients"])):
                     (nickname, muted, country) = channels[c]["clients"][client]
 
@@ -88,28 +166,108 @@ class BEGBot:
                     else:
                         icon = Emoji.LARGE_BLUE_CIRCLE
 
-                    clientlines.append(" {} {} {} {}".format(prefix, icon, nickname, Emoji.flag(country)))
+                    client_lines.append(" {} {} {} {}".format(prefix, icon, nickname, Emoji.flag(country)))
 
                 entries.append("{} {}\n{}".format(Emoji.SPEECH_BALLOON, channels[c]["name"],
-                                                  "\n".join(clientlines)))
+                                                  "\n".join(client_lines)))
 
-            response = "{}{} TeamSpeak 3 {}{}\n{}".format(Emoji.HEAVY_MINUS_SIGN, Emoji.HEAVY_MINUS_SIGN,
-                                                          Emoji.HEAVY_MINUS_SIGN, Emoji.HEAVY_MINUS_SIGN,
-                                                          "\n".join(entries))
+            response = "{}{} TeamSpeak 3 {}{}\n{}".format(
+                Emoji.HEAVY_MINUS_SIGN, Emoji.HEAVY_MINUS_SIGN,
+                Emoji.HEAVY_MINUS_SIGN, Emoji.HEAVY_MINUS_SIGN, "\n".join(entries))
             bot.sendMessage(update.message.chat_id, text=response)
             return True
 
         except ConnectionRefusedError as err:
-            print("TS3 connection failed: {}".format(err))
+            self.logger.error("TS3 connection failed: {}".format(err))
             response = "{} TS3 Error {}".format(Emoji.ANGER_SYMBOL, Emoji.ANGER_SYMBOL)
             bot.sendMessage(update.message.chat_id, text=response)
             return False
 
         except ts3.query.TS3QueryError as err:
-            print("TS3 query failed:", err.resp.error["msg"])
+            self.logger.error("TS3 connection failed: {}".format(err))
             response = "{} TS3 Error {}".format(Emoji.ANGER_SYMBOL, Emoji.ANGER_SYMBOL)
             bot.sendMessage(update.message.chat_id, text=response)
             return False
+
+    def init_db(self):
+        """
+        Gets basic configuration from local database or creates it according
+        to a given schema, if it does not exist.
+
+        @return: True if database creation/connection was successful, False otherwise.
+        """
+        try:
+            new_db = not os.path.exists(self.cfg.db_file)
+            con = sqlite3.connect(self.cfg.db_file)
+            if new_db:
+                self.logger.info("File '{}' not found. Creating new database.".format(self.cfg.db_file))
+
+                with open(self.cfg.db_schema, "rt") as f:
+                    schema = f.read()
+                con.executescript(schema)
+                c = con.cursor()
+                c.execute("insert into user (username, firstname, telegram_id, added, beg, admin) values "
+                          "('Admin', 'Admin', ?, datetime('now'), 1, 1)", (self.cfg.admin_id, ))
+                con.commit()
+                c.close()
+            else:
+                self.logger.info("Using database '{}'.".format(self.cfg.db_file))
+            c = con.cursor()
+            c.execute("insert into session (start, end) values (datetime('now'), datetime('now'));")
+            self.cfg.session_id = c.lastrowid
+            con.commit()
+            c.execute("select telegram_id from user")
+            self.cfg.known_users = {u for u in c.fetchall()}
+            c.close()
+            con.close()
+            return True
+        except KeyError as err:
+            self.logger.error("Error: Missing field in config.json: {}".format(err))
+        return False
+
+    def send_keep_alive(self, session_id):
+        """
+            Saves the current time to the database to keep track of the last known working time in
+            case the bot crashed.
+
+            @param session_id The ID of the current bot session.
+        """
+        con = sqlite3.connect(self.cfg.db_file)
+        con.execute("update session set end = datetime('now') where id=?", (session_id, ))
+        con.commit()
+        con.close()
+
+    @staticmethod
+    def send_message_admin_only(bot, update):
+        """
+        Sends a message to the user, stating that the command is only administrators.
+
+        @param bot: The Telegram bot instance.
+        @param update: The update that was the users command.
+        @return: None
+        """
+        response = "{} Sorry, only for BEGBot administrators.".format(Emoji.ANGER_SYMBOL)
+        bot.sendMessage(update.message.chat_id, text=response)
+
+    @staticmethod
+    def send_message_beg_only(bot, update):
+        """
+        Sends a message to the user, stating that the command is only for BEG members.
+
+        @param bot: The Telegram bot instance.
+        @param update: The update that was the users command.
+        @return: None
+        """
+        response = "{} Sorry, only for Bouncing Egg members.".format(Emoji.ANGER_SYMBOL)
+        bot.sendMessage(update.message.chat_id, text=response)
+
+
+
+
+
+
+
+
 
 """
     # These are example inline and message handlers, not used yet.
--- extended_emoji.py
+++ extended_emoji.py
@@ -1,6 +1,11 @@
+"""
+This class extends the telegram.Emoji class by some additional unicode characters.
+"""
+
 from telegram import Emoji
 from future.utils import bytes_to_native_str as n
 
+# See: https://en.wikipedia.org/wiki/Regional_Indicator_Symbol
 RIS = ["🇦", "🇧", "🇨", "🇩", "🇪", "🇫", "🇬", "🇭", "🇮", "🇯", "🇰", "🇱", "🇲",
        "🇳", "🇴", "🇵", "🇶", "🇷", "🇸", "🇹", "🇺", "🇻", "🇼", "🇽", "🇾", "🇿"]
 
@@ -9,11 +14,19 @@ ABC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 
 ABC2RIS = dict(zip(ABC, RIS))
 
+
 class ExtendedEmoji(Emoji):
     BOX_DRAWINGS_LIGHT_UP_AND_RIGHT = n(b"\xe2\x94\x94")
     BOX_DRAWINGS_LIGHT_VERTICAL_AND_RIGHT = n(b"\xe2\x94\x9c")
 
+    @staticmethod
     def flag(cc):
+        """
+        Returns a countries flag emoji given its ISO 3166-1 alpha-2 country code.
+
+        @param cc: ISO 3166-1 alpha-2 country code of the country.
+        @return: UTF-8 encoded string of the countries flags emoji.
+        """
         if len(cc) != 2:
             return "?"
         return "{}{}".format(ABC2RIS[cc[0]], ABC2RIS[cc[1]])
